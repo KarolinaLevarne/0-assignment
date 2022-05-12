b5b5b743e8113c13f24f742c7a911b9cfa2e446f
KarolinaLevarne <karolina@levarne.nl>
2022-05-12 14:32:11 +0200

    task 2 vue & nuxt - counter component

---
+++ part2-0assignemnt/pages/count.vue
@@ -0,0 +1,17 @@
+<script>
+import Counter from '@/components/Counter';
+
+export default{
+    components: {
+        Counter
+    }
+}
+</script>
+
+<template>
+<div>
+<h1>Here is a child component!</h1>
+  <Counter />
+</div>
+</template>
+
